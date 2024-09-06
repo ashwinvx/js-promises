b5bf4a30ea37c845f4b8d621ea0c15b811e36227
Ashwin <ashwinvx@users.noreply.github.com>
2024-09-06 16:17:38 -0400

    added multiple gifs

--- promises/sketch.js
+++ promises/sketch.js
@@ -1,5 +1,5 @@
 const wordnikAPI =
-    'https://api.wordnik.com/v4/words.json/randomWord?&minLength=5&maxLength=-1&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7';
+    'https://api.wordnik.com/v4/words.json/randomWord?&minLength=3&maxLength=10&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7';
 const giphyAPI =
     'https://api.giphy.com/v1/gifs/search?api_key=HvAxyR0YFMM3J2y9csC73CYtdiYPOTF6&limit=25&offset=0&rating=PG&lang=en&bundle=messaging_non_clips&q=';
 
@@ -13,7 +13,10 @@ function setup() {
         })
         .then(response => response.json())
         .then(data => {
-            createImg(data.data[0].images['original'].url);
+            for (let i = 0; i < data.data.length; i++) {
+                createP(data.data[i].title);
+                createImg(data.data[i].images['original'].url);
+            }
         })
         .catch(error => console.log('error-->', error));
 }
