28186fc7887d022fd64caeb16df8a5efd8db18d1
Ashwin <ashwinvx@users.noreply.github.com>
2024-09-07 14:12:56 -0400

    rewrote function to handle all cases

--- 8.5.8-promises-exercise/numbers.js
+++ 8.5.8-promises-exercise/numbers.js
@@ -1,8 +1,9 @@
 /* 
-    Code to call numbers API which gives some trivia about number
+    Code to call numbers API using promises which gives some trivia about number
 */
 const numbersAPI = 'http://numbersapi.com/';
 const favNumber = 7;
+const numberRange = `1..10,20`;
 
 function setup() {
     noCanvas();
@@ -11,23 +12,19 @@ function setup() {
         callNumbersAPI(favNumber);
         count++;
     }
-    callNumbersAPIwithRange('1..10', 20);
+    callNumbersAPI(numberRange);
 }
 
 function callNumbersAPI(number) {
     fetch(numbersAPI + number + '?json')
         .then(response => response.json())
-        .then(json => createP(json.text))
-        .catch(err => console.error(err));
-}
-
-function callNumbersAPIwithRange(min, max) {
-    fetch(numbersAPI + min + ',' + max + '?json')
-        .then(response => response.json())
         .then(json => {
-            console.log('json-->', json);
-            for (const property in json) {
-                createP(json[property]);
+            if (isNaN(number)) {
+                for (const property in json) {
+                    createP(json[property]);
+                }
+            } else {
+                createP(json.text)
             }
         })
         .catch(err => console.error(err));
